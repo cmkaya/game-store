4af1af8bd85a7c9d18e66e4e106fc890c513b36e
Cem Kaya <cem.kaya@zohomail.com>
2025-04-08 16:38:44 +0300

    feat: add data annotations to Game class

--- Backend/src/GameStore.WebApi/Models/Game.cs
+++ Backend/src/GameStore.WebApi/Models/Game.cs
@@ -1,13 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.WebApi.Models;
 
 public class Game
 {
   public Guid Id { get; set; }
 
+  [Required]
+  [StringLength(50, MinimumLength = 3)]
   public required string Title { get; set; }
 
+  [Required]
+  [StringLength(20, MinimumLength = 3)]
   public required string Genre { get; set; }
 
+  [Range(0.01, 100.00)]
   public decimal Price { get; set; }
 
   public DateOnly ReleaseDate { get; set; }
